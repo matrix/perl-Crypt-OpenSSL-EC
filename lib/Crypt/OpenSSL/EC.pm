e1cab78cc700bd156557ba7e35997f0da6d74657
Gabriele Gristina <matrix@users.noreply.github.com>
2025-07-19 19:15:21 +0200

    updated author, updated EC.xs

--- lib/Crypt/OpenSSL/EC.pm
+++ lib/Crypt/OpenSSL/EC.pm
@@ -375,7 +375,7 @@ our @EXPORT = qw(
 	POINT_CONVERSION_UNCOMPRESSED
 );
 
-our $VERSION = '1.32';
+our $VERSION = '1.33';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
@@ -1304,10 +1304,12 @@ OpenSSL
 =head1 AUTHOR
 
 Mike McCauley, E<lt>mikem@airspayce.comE<gt>
+Gabriele Gristina a.k.a. matrix
 
 =head1 COPYRIGHT AND LICENSE
 
 Copyright (C) 2012 by Mike McCauley
+Copyright (C) 2025 by Gabriele Gristina
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.14.2 or,
